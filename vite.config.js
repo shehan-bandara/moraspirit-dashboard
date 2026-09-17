b31ad1c75133a29281f6c8a2fe164c767b846e35
Shehan Bandara <bandarashehan834@gmail.com>
2026-09-17 21:20:30 +0530

    Configure GitHub Actions deployment workflow

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -8,4 +7,5 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  base: '/moraspirit-dashboard/',
 })
